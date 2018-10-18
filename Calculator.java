f2df96091c8df55109525056f784acad79aff9f5
Suhodoeva Natalia <nnsuhodoeva@mail.ru>
2018-10-18 22:56:04 +0700

    калькулятор(доработанный)

--- Calculator.java
+++ Calculator.java
@@ -2,30 +2,61 @@ import java.util.Scanner;
 public class Calculator{
 	public static void main(String[] arg){
 		Scanner in = new Scanner(System.in);
-        Integer a = 0, b = 0;
+        Double a = 0.0, b = 0.0;
+        Double rez = 0.0;
         while(true){
             System.out.println("1. Enter the first number");
             System.out.println("2. Enter the second number");
             System.out.println("3. Display numbers");
-            System.out.println("4. Add numbers");
-            System.out.println("5. Subtract the second from the first number");
-            System.out.println("6. Multiply numbers");
-            System.out.println("7. Subtract the second from the first number");
+            System.out.println("4. Use the result as the first number");
+            System.out.println("5. Add numbers");
+            System.out.println("6. Subtract the second from the first number");
+            System.out.println("7. Multiply numbers");
+            System.out.println("8. Subtract the second from the first number");
             System.out.println("9. Output");
             Integer i = in.nextInt();
             switch(i){
                 case 1:{
                     System.out.print("a = ");
-                    a = in.nextInt();
+                    a = in.nextDouble ();
                     break;
                 }
                 case 2:{
                     System.out.print("b = ");
-                    b = in.nextInt();
+                    b = in.nextDouble ();
                     break;
                 }
                 case 3:{
                     System.out.println("a = "+ a +" b = "+b);
+                    break;
+                }
+                case 4:{
+                    a = rez;
+                    System.out.println("a = "+ rez);
+                    break;
+                }
+                case 5:{
+                    rez = a + b;
+                    System.out.println("a = "+ a +" b = "+b);
+                    System.out.println("rez = "+ rez);
+                    break;
+                }
+                case 6:{
+                    rez = a - b;
+                    System.out.println("a = "+ a +" b = "+b);
+                    System.out.println("rez = "+ rez);
+                    break;
+                }
+                case 7:{
+                    rez = a * b;
+                    System.out.println("a = "+ a +" b = "+b);
+                    System.out.println("rez = "+ rez);
+                    break;
+                }
+                case 8:{
+                    rez = a / b;
+                    System.out.println("a = "+ a +" b = "+b);
+                    System.out.println("rez = "+ rez);
                     break;
                 }
                 case 9: {
