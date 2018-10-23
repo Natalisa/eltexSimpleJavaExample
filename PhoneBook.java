8fe25bb253b5f6a21d8032812097f0770c303c7b
Suhodoeva Natalia <nnsuhodoeva@mail.ru>
2018-10-23 19:07:48 +0700

    недоработанный класс телефонный справочник

---
+++ PhoneBook.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+public class PhoneBook{
+    ArrayList<Person> phoneBook = new ArrayList<>();
+    public PhoneBook(Person newPerson){
+        this.phoneBook.add(newPerson);
+    }
+    public void addPerson(Person p){
+        this.phoneBook.add(p);
+    }
+    public Person getPerson(String name){
+        for (Person element: this.phoneBook){
+            if(element.name.equals(name)){
+                return element;
+            }
+        }
+    }
+    
+    public static void main(String[] arg){
+        Person newP = new Person("Nata","56433");
+        PhoneBook phoneBk = new PhoneBook(new Person("Nata","56433"));
+        phoneBk.addPerson(newP);
+        System.out.println(phoneBk.phoneBook);
+    }
+}
