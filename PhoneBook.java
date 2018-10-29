e894244e4619468669de665bf5d523e1ebb0b4b1
Suhodoeva Natalia <nnsuhodoeva@mail.ru>
2018-10-30 00:00:56 +0700

    Доработка телефонного справочника

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 public class PhoneBook{
     ArrayList<Person> phoneBook = new ArrayList<>();
+    public PhoneBook(){
+        
+    }
     public PhoneBook(Person newPerson){
         this.phoneBook.add(newPerson);
     }
@@ -13,16 +16,27 @@ public class PhoneBook{
                 return element;
             }
         }
+        return null;
     }
     public void printPhoneBook(){
          for (Person element: this.phoneBook){
-               System.out.println(element.name);   
+               System.out.print(element.name+": ");
+             for(String phone: element.phone){
+                 System.out.println(phone);
+             }
         }
+    }
+    public void addPhone(String name, String number){
+        Person tmp = getPerson(name);
+        tmp.addPhone(number);
     }
     public static void main(String[] arg){
         Person newP = new Person("Nata","56433");
-        PhoneBook phoneBk = new PhoneBook(new Person("Nata","56433"));
+        PhoneBook phoneBk = new PhoneBook();
         phoneBk.addPerson(newP);
+        phoneBk.addPerson(new Person("Ira","12314"));
+        phoneBk.addPhone("Nata","345436");
+        phoneBk.addPhone("Nata","56844");
         phoneBk.printPhoneBook();
     }
 }
